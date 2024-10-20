be42444e3531ba39cb1f88d5c0d11b9e3052f8bb
evelyne <evelyneujeneza@gmail.com>
2024-10-20 10:10:37 +0000

    added migrations

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8l7s!1l4bf-vt2l-_9fw&$67o7%081bb4w4_9ef-)ur_t7m$j8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Evelyne.pythonanywhere.com']
 
